4287467a81639827cb8cf426a668ec8d4814981f
nizarPdC <60057617+nizarPdC@users.noreply.github.com>
2024-03-30 14:28:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// const canvas = document.querySelector("canvas");
+// const ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = "rgb(255 0 0 / 25%)";
+
+// ctx.beginPath();
+// ctx.arc(100, 100, 70, Math.PI, 2*(Math.PI));
+// ctx.fill()
+
+// ctx.fillStyle = "rgb(255 0 0 / 50%)";
+
+// ctx.beginPath();
+// ctx.arc(200, 180, 60, Math.PI, 2*(Math.PI));
+// ctx.fill()
+
+// ctx.fillStyle = "rgb(255 0 0)";
+// ctx.beginPath();
+// ctx.arc(200, 180, 50, Math.PI, 2*(Math.PI));
+// ctx.fill()
+
+
